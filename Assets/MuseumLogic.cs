350c3b7710a7c94bc92054f05c7ceee9bc32ecff
Keelan <keelan.downton@gmail.com>
2017-12-28 10:07:47 -0500

    Ensured welcome screen is always facing user

--- Assets/MuseumLogic.cs
+++ Assets/MuseumLogic.cs
@@ -7,12 +7,18 @@ public class MuseumLogic : MonoBehaviour {
 	GameObject obj;
 
 	public GameObject player;
+	public GameObject camera;
 	public GameObject startWaypoint;
+	public GameObject welcomeScreen;
+
+	private float fixedWelcomeScreenXRotation;
+
 	Vector3 nextPosition;
 	// Use this for initialization
 	void Start () {
-		player = player.transform.parent.gameObject;
+		player = camera.transform.parent.gameObject;
 		player.transform.position = startWaypoint.transform.position;
+		fixedWelcomeScreenXRotation = welcomeScreen.transform.rotation.eulerAngles.x;
 	}
 
 	public void SetNextPosition(Vector3 position){
@@ -32,5 +38,15 @@ public class MuseumLogic : MonoBehaviour {
 	void Update () {
 		RenderSettings.skybox.SetFloat("_Exposure", Mathf.Sin(Time.time * Mathf.Deg2Rad * 20)*Mathf.Sin(Time.time * Mathf.Deg2Rad * 20)/2f + 0.75f);
 		RenderSettings.skybox.SetFloat("_Rotation", Time.time * .2f); 
+
+		if (welcomeScreen.activeSelf) {
+			welcomeScreen.transform.rotation = Quaternion.Euler(
+				new Vector3(
+					fixedWelcomeScreenXRotation, 
+					camera.transform.rotation.eulerAngles.y, 
+					camera.transform.rotation.eulerAngles.z
+				)
+			);
+		}
 	}
 }
